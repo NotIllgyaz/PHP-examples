dc76fd429b76a5078e560b8f44e2070b360a615d
Luca <lucapols@ziggo.nl>
2021-04-18 13:29:06 +0200

    example 1

---
+++ json-format-example/index.php
@@ -0,0 +1,18 @@
+<?php
+// PHP JSON-Format Example (using json_decode + some other stuff.)
+
+// JSON data
+$json_data = [
+    "bruh" => "yes",
+    "bruh2" => [
+        "bruh3" => "yes"
+    ]
+]
+
+// Show the $json_data
+
+echo json_encode($json_data);
+
+// This is how JSON formats work in PHP.
+// This script is written by thisclubhouse-is-weird, and feel free to remove the quotes and use this!
+?>
